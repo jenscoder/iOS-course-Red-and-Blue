10ed2efbd69887e547d275602406b0d1b60af732
Jens Van Steen <jensvansteen@iMac2010.local>
2015-12-18 22:38:24 +0100

    added files for RedAndBlue

--- Red and Blue/ViewController.swift
+++ Red and Blue/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Headline: UIImageView!
+    @IBOutlet weak var RedBomb: UIImageView!
+    @IBOutlet weak var BlueBomb: UIImageView!
+    @IBOutlet weak var Background: UIImageView!
+    @IBOutlet weak var RedButton: UIButton!
+    @IBOutlet weak var BlueButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func HideRed(sender: AnyObject) {
+        RedBomb.hidden = true
+        BlueBomb.hidden = false
+        Headline.hidden = false
+        Background.hidden = false
+    }
+    @IBAction func HideBlue(sender: AnyObject) {
+        BlueBomb.hidden = true
+        RedBomb.hidden = false
+        Background.hidden = false
+       Headline.hidden = false
+    }
 
 }
 
